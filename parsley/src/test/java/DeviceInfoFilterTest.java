4943c90a989a6d5f6d6e6adc42a631231fda8f70
Mats Ljunggren <mljunggren@gmail.com>
2014-05-13 14:37:38 -0400

    Added Test Class for DeviceInfoFilter

---
+++ parsley/src/test/java/DeviceInfoFilterTest.java
@@ -0,0 +1,16 @@
+package helloworld.filters;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeviceInfoFilterTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals("This must be true", 50, 50);
+		//assertEquals("This must be true", 50, 60);
+	}
+
+}
